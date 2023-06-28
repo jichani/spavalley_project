349b213b7f14251d803dfd866a611aa6a8d1ad06
jichani <jichan7571@naver.com>
2023-06-28 18:08:53 +0900

    버튼 추가 반응형은 아직

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    autoplay: {
+        delay: 2000,
+    },
+    // controller: {
+    //     inverse: true,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+});
